365cbfa8859d11429139cfbafa5f623ae4ad2e66
Brandon LaFave <blafave@hollandamerica.com>
2017-04-18 14:22:47 -0700

    Add date to weather tiles

--- src/components/WeatherTile/WeatherTile.js
+++ src/components/WeatherTile/WeatherTile.js
@@ -3,16 +3,24 @@ import './WeatherTile.css';
 
 class WeatherTile extends Component {
 
+	getDate(time) {
+		let d = new Date(time * 1000)
+		let d2 = d.toString()
+		return d2.substring(0, 15)
+	}
+
 	render() {
 
 		let weatherTiles = this.props.forcast.map((day, index) => {
 			return (
 				<div className="WeatherTile" key={index}>
-					<p>{day.summary}</p>
+					<h2>{ this.getDate(day.time) }</h2>
+					
 					<div className={ day.icon ? day.icon : 'default-icon' }></div>
 					<div className="pull-right">
-						<p>Min: {day.temperatureMin}˚</p>
-						<p>Max: {day.temperatureMax}˚</p>
+						<p>{day.summary}</p>
+						<p>Low: {day.temperatureMin}˚</p>
+						<p>High: {day.temperatureMax}˚</p>
 					</div>
 				</div>
 			);
