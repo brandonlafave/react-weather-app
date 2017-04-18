e0bc168966d13e75a8371851505cec716f71b13e
Brandon LaFave <blafave@hollandamerica.com>
2017-04-18 10:48:49 -0700

    Remove App Intro section and styles from app

--- src/App.js
+++ src/App.js
@@ -36,9 +36,6 @@ class App extends Component {
 			  		<img src={logo} className="App-logo" alt="logo" />
 			  		<h2>Welcome to React</h2>
 				</div>
-				<p className="App-intro">
-			  		To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
 				<WeatherTile forcast={this.state.dailyForcast} />
 				<footer>
 					<a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
