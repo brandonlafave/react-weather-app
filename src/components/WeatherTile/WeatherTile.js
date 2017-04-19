58f09a363fba22341839ff8f07ee1671ad2e9ab2
Brandon LaFave <blafave@hollandamerica.com>
2017-04-18 20:17:50 -0700

    Remove unnecessary divs

--- src/components/WeatherTile/WeatherTile.js
+++ src/components/WeatherTile/WeatherTile.js
@@ -4,8 +4,11 @@ import './WeatherTile.css';
 class WeatherTile extends Component {
 
 	getDate(time) {
+		// Converts timestamp returned from the API into milliseconds 
 		let d = new Date(time * 1000)
+		// Converts the Date object into a string that can be rendered to the UI
 		let d2 = d.toString()
+		// Removes hours, minutes, seconds, and timezone info from the date string
 		return d2.substring(0, 15)
 	}
 
@@ -14,21 +17,20 @@ class WeatherTile extends Component {
 		let weatherTiles = this.props.forcast.map((day, index) => {
 			return (
 				<div className="WeatherTile" key={index}>
-					<h2>{ this.getDate(day.time) }</h2>
-					
-					<div className={ day.icon ? day.icon : 'default-icon' }></div>
-					<div className="pull-right">
-						<p>{day.summary}</p>
-						<p>Low: {day.temperatureMin}˚</p>
-						<p>High: {day.temperatureMax}˚</p>
+					<div className="margin-lr">
+						<h2>{ this.getDate(day.time) }</h2>
 					</div>
+					<div className={ day.icon ? day.icon : 'default-icon' }></div>
+					<p className="fs-small">{ day.summary }</p>
+					<p className="fs-small">Low: <span className="fs-small">{ day.temperatureMin }˚</span></p>
+					<p className="fs-small">High: <span className="fs-small">{ day.temperatureMax }˚</span></p>
 				</div>
 			);
 		});
 
 		return (
 			<div className="WeatherTileContainer">
-				{weatherTiles}
+				{ weatherTiles }
 		  	</div>
 		);
 	}
